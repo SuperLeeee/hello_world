ab8404b2c8198502de14cc9a5544688816314eb1
淩超 <liyingchao.lyc@alibaba-inc.com>
2021-05-24 16:31:00 +0800

    second commit

--- src/MyTest.java
+++ src/MyTest.java
@@ -1,5 +1,11 @@
 public class MyTest {
     public static void main(String[] args) {
+        int a = 0;
+        a++;
+        a++;
+        a *= a;
+        a--;
+        System.out.println("hello hello");
         System.out.println("Hello world");
     }
 }
